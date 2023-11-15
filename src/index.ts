7508db0c11c0dcfbe5b745fdb1672c10b001aeab
Vladyslav Arkavenko <vladyslavarkavenko@gmail.com>
2023-11-15 15:28:45 +0100

    Plugin base setup, integration with registry contract completed, basic tests written

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,50 @@
-import { Web3PluginBase } from "web3";
+import { Web3PluginBase, Contract, Address } from "web3";
 
-export class TemplatePlugin extends Web3PluginBase {
-  public pluginNamespace = "template";
+import registryContractAbi from "./artifacts/registryContractAbi";
 
-  public test(param: string): void {
-    console.log(param);
+type IpfsPluginOptions = {
+  registryContractAddress: Address;
+};
+
+export class IpfsPlugin extends Web3PluginBase {
+  public pluginNamespace = "ipfs";
+
+  private registryContractInstance?: Contract<typeof registryContractAbi>;
+  private readonly registryContractAddress: Address;
+
+  constructor({ registryContractAddress }: IpfsPluginOptions) {
+    super();
+    this.registryContractAddress = registryContractAddress;
+  }
+
+  // We define this as getter, instead of initialising in constructor, because we need to wait for the Web3 to register plugin first.
+  private get registryContract(): Contract<typeof registryContractAbi> {
+    if (!this.registryContractInstance) {
+      this.registryContractInstance = new Contract(
+        registryContractAbi,
+        this.registryContractAddress
+      );
+      this.registryContractInstance.link(this);
+    }
+
+    return this.registryContractInstance;
+  }
+
+  public async uploadFile(): Promise<void> {
+    const cid = "test";
+    await this.registryContract.methods.store(cid).send();
+  }
+
+  public async listAllCIDs(ownerAddress: Address): Promise<void> {
+    const events = await this.registryContract.getPastEvents("CIDStored", {
+      filter: { owner: ownerAddress },
+    });
+    console.log(events);
   }
 }
 
-// Module Augmentation
 declare module "web3" {
   interface Web3Context {
-    template: TemplatePlugin;
+    ipfs: IpfsPlugin;
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,60 @@
-import { Web3, core } from "web3";
-import { TemplatePlugin } from "../src";
+import { Web3, core, Address } from "web3";
 
-describe("TemplatePlugin Tests", () => {
-  it("should register TemplatePlugin plugin on Web3Context instance", () => {
-    const web3Context = new core.Web3Context("http://127.0.0.1:8545");
-    web3Context.registerPlugin(new TemplatePlugin());
-    expect(web3Context.template).toBeDefined();
+import { IpfsPlugin } from "../src";
+
+jest.setTimeout(50000);
+
+const providerUrl = "https://ethereum-sepolia.publicnode.com";
+const registryContractAddress = "0xA683BF985BC560c5dc99e8F33f3340d1e53736EB";
+const testAccountPrivateKey = "0xcc59ca865cd8264614bc87f6f6c102bab06790489701d0f0550445c476d46f9f";
+
+describe("IpfsPlugin tests", () => {
+  it("Should register IpfsPlugin plugin on Web3Context instance", () => {
+    const web3Context = new core.Web3Context(providerUrl);
+    web3Context.registerPlugin(new IpfsPlugin({ registryContractAddress }));
+    expect(web3Context.ipfs).toBeDefined();
   });
 
-  describe("TemplatePlugin method tests", () => {
-    let consoleSpy: jest.SpiedFunction<typeof global.console.log>;
-
+  describe("IpfsPlugin method uploadFile", () => {
     let web3: Web3;
 
     beforeAll(() => {
-      web3 = new Web3("http://127.0.0.1:8545");
-      web3.registerPlugin(new TemplatePlugin());
+      web3 = new Web3(providerUrl);
+
+      const testAccount = web3.eth.accounts.privateKeyToAccount(
+        testAccountPrivateKey
+      );
+      web3.eth.accounts.wallet.add(testAccount);
+      web3.defaultAccount = testAccount.address;
+
+      web3.registerPlugin(new IpfsPlugin({ registryContractAddress }));
+    });
+
+    it("Should call IpfsPlugin method uploadFile successfully", async () => {
+      try {
+        await web3.ipfs.uploadFile();
+        expect(true).toBe(true);
+      } catch (err) {
+        expect(err).toBeUndefined();
+      }
+    });
+  });
+
+  describe("IpfsPlugin method listAllCIDs", () => {
+    let web3: Web3;
+    let testAccountAddress: Address;
+    let consoleSpy: jest.SpiedFunction<typeof global.console.log>;
+
+    beforeAll(() => {
+      web3 = new Web3(providerUrl);
+
+      const testAccount = web3.eth.accounts.privateKeyToAccount(
+        testAccountPrivateKey
+      );
+      testAccountAddress = testAccount.address;
+
+      web3.registerPlugin(new IpfsPlugin({ registryContractAddress }));
+
       consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
     });
 
@@ -23,9 +62,9 @@ describe("TemplatePlugin Tests", () => {
       consoleSpy.mockRestore();
     });
 
-    it("should call TempltyPlugin test method with expected param", () => {
-      web3.template.test("test-param");
-      expect(consoleSpy).toHaveBeenCalledWith("test-param");
+    it("Should call IpfsPlugin method listAllCIDs successfully", async () => {
+      await web3.ipfs.listAllCIDs(testAccountAddress);
+      expect(consoleSpy).toHaveBeenCalled();
     });
   });
 });
